7abce1a2f33a668b50cf37a26d9df823d1b159a5
Oleksandr Moshorovskyi <46052383+aleksandrmoshorovskyi@users.noreply.github.com>
2024-04-21 11:08:40 +0300

    init

--- Lesson-18-GCD/Printer.swift
+++ Lesson-18-GCD/Printer.swift
@@ -9,6 +9,38 @@ import Foundation
 
 class Printer {
 
+    var completion: (((text: String, count: Float)) -> ())?
+    
+    func printSomeTextWithCounter(text: String) {
+        
+        var progresInt: Float = 0.0
+        var counter = -99999
+        
+        while counter < 99999 {
+            counter += 1
+            let text = "\(text) - \(counter)"
+            //для плавності
+            if counter % 1000 == 0 {
+                
+                switch counter {
+                case -99999...0:
+                    progresInt = 0.5 - (Float(counter * (-1)) * 50.0) / 99999 / 100
+                case 0:
+                    progresInt = 0.5
+                case 0...99999:
+                    progresInt = 0.5 + (Float(counter) * 50.0) / 99999 / 100
+                case 99999:
+                    progresInt = 1
+                default:
+                    progresInt = 0
+                }
+                
+                completion?((text, progresInt))
+            }
+            debugPrint(text)
+        }
+    }
+    
     func printSomeText(text: String) {
         
         var counter = -99999
--- Lesson-18-GCD/ViewController.swift
+++ Lesson-18-GCD/ViewController.swift
@@ -5,21 +5,85 @@
 //  Created by Aleksandr Moroshovskyi on 18.04.2024.
 //
 
+/*
+ //ченсто використований код
+ //виконуємо щось async,
+ //потім передаємо в main - для змін на view, наприклад
+ 
+ DispatchQueue.global(qos: .default).async {
+     Printer().printSomeText(text: "🛑 default")
+     
+     DispatchQueue.main.async {
+         Printer().printSomeText(text: "🎃 default")
+     }
+ }
+ */
+
 import UIKit
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var btn: UIButton!
     
+    //оголошення своєї черги
+    //let myQueue = DispatchQueue(label: "MyOwnQueue", qos: .default)
+    let myQueue = DispatchQueue(label: "MyOwnQueue", attributes: .concurrent)
+    
+    //without .concurrent - it's Serial
+    let myQueueSerial = DispatchQueue(label: "MyOwnQueue")
+    
+    //одна операція на весь клас
+    let mySingleOperationQueue = OperationQueue()
+    let mySingleOperationQueueSerial = OperationQueue()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //if 1 - it's serial
+        mySingleOperationQueueSerial.maxConcurrentOperationCount = 1
+        
+        //test
+        progressView.progress = 0
+    }
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    @IBAction func thirdBtnAction(_ sender: Any) {
+        
+        debugPrint("🟤 Third button did tap")
+        
+        //Operation
+        //runOperation()
+        
+        test()
+            
+        debugPrint("✔️ Third button action completed")
     }
     
     @IBAction func secondButtonAction(_ sender: Any) {
         
+        debugPrint("🚛 BIG second button did tap")
+        
         //runSomeInGLobalUserDefaultSYNC()
-        runSomeInGLobalUserDefault_ASYNC_ASYNC()
+        //runSomeInGLobalUserDefault_ASYNC_ASYNC()
+        
+        //.concurrent
+        //runSomeCode1()
+        //runSomeCode2()
+        //runSomeCode3()
+        
+        //.serial
+        //runSomeCode1_async_mainasync()
+        //runSomeCode2_async_mainasync()
+        //runSomeCode3_async_mainasync()
+        
+        //DispatchGroup
+        //runMultipleCodeInClobal()
+        
+        runMultipleCodeInClobalFor()
+        
+        debugPrint("✅ BIG secondButtonAction completed")
     }
     
     @IBAction func btnAction(_ sender: Any) {
@@ -134,9 +198,11 @@ class ViewController: UIViewController {
     func runSomeInGLobalUserDefault_SYNC_SYNC() {
         
         /*
-         ⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️
+         ⛔️⛔️⛔️⛔️⛔️ ⛔️⛔️⛔️⛔️⛔️
+         
          ⛔️ SYNC - SYNC ⛔️
-         ⛔️⛔️⛔️⛔️⛔️⛔️⛔️⛔️
+         
+         ⛔️⛔️⛔️⛔️⛔️ ⛔️⛔️⛔️⛔️⛔️
          
          DEADLOCK
          
@@ -171,6 +237,153 @@ class ViewController: UIViewController {
     
     //    ..1:12:55..
     
-    //TODO
+    //приклад, три func у одній черзі
+    func runSomeCode1() {
+        //DispatchQueue.global(qos: .default).async {
+        myQueue.async {
+            Printer().printSomeText(text: "1️⃣ default")
+        }
+    }
+    
+    func runSomeCode2() {
+        //DispatchQueue.global(qos: .default).async {
+        myQueue.async {
+            Printer().printSomeText(text: "2️⃣ default")
+        }
+    }
+    
+    func runSomeCode3() {
+        //DispatchQueue.global(qos: .default).async {
+        myQueue.async {
+            Printer().printSomeText(text: "3️⃣ default")
+        }
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///
+    ///виконаємо код async і передамо результат у myQueueSerial
+    func runSomeCode1_async_mainasync() {
+        DispatchQueue.global(qos: .default).async {
+            Printer().printSomeText(text: "1️⃣ default")
+            self.myQueueSerial.sync {
+                debugPrint("Code 1 has been finished")
+            }
+        }
+    }
+    
+    func runSomeCode2_async_mainasync() {
+        DispatchQueue.global(qos: .default).async {
+            Printer().printSomeText(text: "2️⃣ default")
+            self.myQueueSerial.sync {
+                debugPrint("Code 2 has been finished")
+            }
+        }
+    }
+    
+    func runSomeCode3_async_mainasync() {
+        DispatchQueue.global(qos: .default).async {
+            Printer().printSomeText(text: "3️⃣ default")
+            self.myQueueSerial.sync {
+                debugPrint("Code 3 has been finished")
+            }
+        }
+    }
+    
+    
+    //DispatchGroup
+    func runMultipleCodeInClobal() {
+        
+        let dispatchGroup = DispatchGroup()
+        
+        dispatchGroup.enter()
+        let task1 = DispatchWorkItem {
+            debugPrint("task1")
+            dispatchGroup.leave()
+        }
+        
+        dispatchGroup.enter()
+        let task2 = DispatchWorkItem {
+            debugPrint("task2")
+            dispatchGroup.leave()
+        }
+        
+        task2.perform()
+        task1.perform()
+        
+        dispatchGroup.wait()
+        
+        debugPrint("Task1 and Task2 completed!")
+    }
+    
+    func runMultipleCodeInClobalFor() {
+        
+        let dispatchGroup = DispatchGroup()
+        var tasks: [DispatchWorkItem] = []
+        
+        for i in 0 ... 99 {
+            dispatchGroup.enter()
+            let task = DispatchWorkItem {
+                debugPrint("task \(i)")
+                dispatchGroup.leave()
+            }
+            tasks.append(task)
+        }
+        
+        tasks.forEach { $0.perform() }
+        
+        dispatchGroup.wait()
+        
+        debugPrint("ALL Tasks completed!")
+    }
+    
+    //Operation
+    func runOperation() {
+        
+        let operationQueue = OperationQueue()
+        //count of operations
+        //if 1 - serial
+        //if 2 and >2 - concurrent
+        operationQueue.maxConcurrentOperationCount = 2
+        
+        //Operation
+        
+        operationQueue.addOperation {
+            Printer().printSomeText(text: "🚗 operation 1")
+        }
+        
+        operationQueue.addOperation {
+            Printer().printSomeText(text: "🚙 operation 2")
+        }
+        
+        operationQueue.addOperation {
+            Printer().printSomeText(text: "🚕 operation 3")
+        }
+        
+        operationQueue.addOperation {
+            Printer().printSomeText(text: "🚓 operation 4")
+        }
+        
+        //помітка для системи, що операція може бути скасована
+        //operationQueue.cancelAllOperations()
+    }
+    
+    //test виводу статусу завантаження на екран
+    //ПОГАНИЙ ПРИКЛАД - ЯК НЕ ТРЕБА РОБИТИ)))
+    func test() {
+        
+        let print = Printer()
+        print.completion = { [weak self] params in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                self?.counterLabel.text = params.text
+                self?.progressView.progress = params.count
+            }
+        }
+        
+        DispatchQueue.global(qos: .default).async {
+            
+            print.printSomeTextWithCounter(text: "🚧 test")
+        }
+    }
+    
 }
 
